744511a9e71534686beb9f392a7cb6ecce7791d7
AbytovD <abytov247@gmail.com>
2026-02-06 19:19:53 +0600

    updat

--- src/shared/api/client.js
+++ src/shared/api/client.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const API_BASE = 'http://127.0.0.1:8000/api';
+const API_BASE =
+  process.env.NODE_ENV === 'production'
+    ? 'https://rahman.tw1.su/api'
+    : 'http://127.0.0.1:8000/api';
 
 export const apiClient = axios.create({
   baseURL: API_BASE,

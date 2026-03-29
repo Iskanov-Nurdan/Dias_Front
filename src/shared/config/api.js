825d7de14df6edcc426ce5f57d7406ec6f4a6ae9
Iskanov-Nurdan <goodhuman954@gmail.com>
2026-03-29 23:24:24 +0600

    обновил1234

--- src/shared/config/api.js
+++ src/shared/config/api.js
@@ -3,3 +3,5 @@ export const API_BASE = (process.env.REACT_APP_API_URL || DEFAULT_API).replace(/
 
 const DEFAULT_WS = '';
 export const WS_URL = (process.env.REACT_APP_WS_URL || DEFAULT_WS).trim() || undefined;
+
+// goof

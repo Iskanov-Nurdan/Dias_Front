0bcec020b2b6f11547a722244d1f100c951393b0
Iskanov-Nurdan <goodhuman954@gmail.com>
2026-04-04 06:13:52 +0600

    update1245678910

--- src/shared/config/api.js
+++ src/shared/config/api.js
@@ -1,7 +1,7 @@
-const DEFAULT_API = 'https://nur.tw1.ru';
+const DEFAULT_API = window.location.hostname === 'nur.tw1.ru' 
+    ? 'http://nur.tw1.ru' 
+    : 'http://72.56.244.50';
 export const API_BASE = (process.env.REACT_APP_API_URL || DEFAULT_API).replace(/\/?$/, '/');
 
 const DEFAULT_WS = '';
 export const WS_URL = (process.env.REACT_APP_WS_URL || DEFAULT_WS).trim() || undefined;
-
-// goof
